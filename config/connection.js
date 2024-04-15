82a014a413153e036a87063bbd9cf5a1b2354733
Xydra <theminecraftgurru@gmail.com>
2024-04-15 15:32:15 -0400

    Setup connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,11 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
+
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+module.exports = sequelize;
