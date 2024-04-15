9088dd01b2dd952f71639c9434dd32650e993c34
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-15 15:52:08 -0400

    set up handlebars engine

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const routes = require('./controllers/index');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
