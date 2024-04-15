0ffab7d684b1883f7352b4c2bdfdd15124273b3f
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-15 14:28:38 -0400

    created followed anime route in the controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require('./api/index.js');
 const homeRoutes = require('./home-routes.js');
+const followedRoute = require('./followed-routes.js');
 
 router.use('/', homeRoutes);
+router.use('/followed', followedRoute);
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
