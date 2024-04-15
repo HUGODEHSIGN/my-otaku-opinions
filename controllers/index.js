ed567dce7dd6c7cb63205242805ae5cfc6fff01a
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-15 14:43:39 -0400

    created category-route.js file

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/index.js');
 const homeRoutes = require('./home-routes.js');
 const followedRoute = require('./followed-routes.js');
+const categoryRoute = require('./category-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/followed', followedRoute);
+router.use('/category', categoryRoute);
 
 router.use('/api', apiRoutes);
 
