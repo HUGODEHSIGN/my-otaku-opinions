bf420d047167976430fb1876df178244a32da8b0
Lovertab <lovertabrown@gmail.com>
2024-04-15 16:55:23 -0400

    added the checkpassword

--- models/User.js
+++ models/User.js
@@ -2,3 +2,8 @@ const { Model, DataType } = require('sequelize');
 const bcrypt = reqquire('bcrypt');
 const sequelize = require('../config/connection');
 
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
