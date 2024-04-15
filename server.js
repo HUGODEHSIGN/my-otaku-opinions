52cfc1904d4b8bfe3baceed0835451832b7f6cb7
Xydra <theminecraftgurru@gmail.com>
2024-04-15 15:37:55 -0400

    Added sequelize initialization

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.send('Howdy, World');
+  res.send('Howdy, World');
 });
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}...`);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connected to database sucessfully');
+
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}...`);
+    });
+  })
+  .catch((err) => {
+    console.log('Database connection error:', err);
+  });
