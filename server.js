ca1c75627d723a23e276591862890b7f0f460d39
Xydra <theminecraftgurru@gmail.com>
2024-04-15 16:28:00 -0400

    Used sever.js setup from lesson 14

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
-const routes = require('./controllers/index');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Howdy, World');
-});
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({});
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connected to database sucessfully');
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-    app.listen(PORT, () => {
-      console.log(`listening on port ${PORT}...`);
-    });
-  })
-  .catch((err) => {
-    console.log('Database connection error:', err);
-  });
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}...`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
