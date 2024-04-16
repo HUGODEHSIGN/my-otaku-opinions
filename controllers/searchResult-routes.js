8597ca6451e297469ef133c975dc72c2bba3b4ee
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-16 14:27:27 -0400

    displays searched anime to searchResults handlebar page

--- controllers/searchResult-routes.js
+++ controllers/searchResult-routes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
+  console.log(req.query.q);
   try {
     // this url only gets the top 25 results, change later if we want pagination
+    const queryParams = req.query.q;
     const result = await fetch(
-      'https://api.jikan.moe/v4/anime?q=naruto&order_by=popularity&sort=asc'
+      `https://api.jikan.moe/v4/anime?q=${queryParams}&order_by=popularity&sort=asc`
     );
     const apiData = await result.json();
-    console.log(animeArray);
+    console.log(apiData);
     res.status(200).render('searchResults', { apiData });
   } catch (err) {
     console.error(err);
