bf861b350996ff840dcbbbee6f164e21070b2c3d
ECS <ethansantana93@gmail.com>
2024-04-16 11:51:34 -0400

    adds signupform function to login.js not finished

--- public/js/login.js
+++ public/js/login.js
@@ -18,6 +18,21 @@ const loginFormHandler = async (event) => {
   }
 };
 
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+  const username = document.querySelector('#signup-username').value.trim();
+  const email = document.querySelector('#signup-email').value.trim();
+  const password = document.querySelector('#signup-password').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/signup', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+};
+
 document
   .querySelector('#login-form')
   .addEventListener('submit', loginFormHandler);
