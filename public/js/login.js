6b3c4879abe7766ad5b5869209c9904a9e3f683f
ECS <ethansantana93@gmail.com>
2024-04-16 11:12:24 -0400

    added login and logout js files in a new public folder

---
+++ public/js/login.js
@@ -0,0 +1,5 @@
+const loginForm = async (event) => {
+  event.preventDefault();
+  const email = document.querySelector('#email-log').value.trim();
+  const password = document.querySelector('#password-log').value.trim();
+};
