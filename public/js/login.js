949707c1f692c8a327a27c11d7a8831314c1e2ad
ECS <ethansantana93@gmail.com>
2024-04-16 11:13:31 -0400

    added logFrom function in Login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,4 +2,18 @@ const loginForm = async (event) => {
   event.preventDefault();
   const email = document.querySelector('#email-log').value.trim();
   const password = document.querySelector('#password-log').value.trim();
+
+  if (email && password) {
+    const response = await fetch('TODO:', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log in.');
+  }
 };
