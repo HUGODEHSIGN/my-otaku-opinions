8026426767373121a5461574096b7d987ae5fcba
Lovertab <lovertabrown@gmail.com>
2024-04-16 11:36:43 -0400

    added const and init to useranime

--- models/UserAnime.js
+++ models/UserAnime.js
@@ -1,1 +1,10 @@
 // this is to join the table to create a Many to Many relationship
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserAnime extends Model {}
+
+UserAnime.init(
+    {
+        
+    })
