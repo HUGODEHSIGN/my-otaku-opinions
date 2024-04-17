e76dcdca5dd83b05f87fb495a0ad1760be223b73
Lovertab <lovertabrown@gmail.com>
2024-04-17 10:24:10 -0400

    added the router.us for logout and const logout route as well

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const loginRoutes = require('./loginRoutes.js');
-const logoutRoutes = require('./logoutRoutes.js');
 const signupRoutes = require('./signupRoutes.js');
+const logoutRoutes = require('./logoutRoutes.js');
 
 router.use('/login', loginRoutes);
 router.use('/signup', signupRoutes);
-
+router.use('./logout', logoutRoutes);
 module.exports = router;
