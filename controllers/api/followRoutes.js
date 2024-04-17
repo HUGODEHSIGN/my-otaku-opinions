3c39227b15b8b86501e6e406ff78e0fcc2d6acb1
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-17 14:42:14 -0400

    cleaned up model, added fetch request to api in followRoutes.js

--- controllers/api/followRoutes.js
+++ controllers/api/followRoutes.js
@@ -1,3 +1,24 @@
-router.post('/', (req, res) => {
+const router = require('express').Router();
+const { Anime, UserAnime } = require('../../models');
+
+router.post('/', async (req, res) => {
+  const malId = req.body.malId;
+
+  const apiRes = await fetch(`https://api.jikan.moe/v4/anime/${malId}`);
+  const apiData = await apiRes.json();
+  const { images, title, synopsis } = await apiData.data;
+  console.log(apiAnime);
+
+  const [anime, created] = await Anime.findOrCreate({
+    where: { api_id: malId },
+    defaults: {
+      title,
+      synopsis,
+      img_url: images.webp.image_url,
+      api_id: malId,
+    },
+  });
   console.log(req.body);
 });
+
+module.exports = router;
--- public/js/follow.js
+++ public/js/follow.js
@@ -1,7 +1,15 @@
 const followButtons = document.querySelectorAll('.follow-btn');
 
 async function handleFollowAnime(e) {
-  console.log(e.target.dataset.animeId);
+  //   console.log(e.target.dataset.malId);
+  const malId = e.target.dataset.malId;
+  const res = await fetch('/api/follow', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ malId }),
+  });
 }
 
 followButtons.forEach((button) =>
