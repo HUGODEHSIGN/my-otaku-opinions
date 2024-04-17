0b571349b03c866418e67730e44c168f3ff2889d
ECS <ethansantana93@gmail.com>
2024-04-17 10:33:20 -0400

    changes the logic for login js, only use username and pass

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,12 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  const email = document.querySelector('#login-email').value.trim();
+  const username = document.querySelector('#login-username').value.trim();
   const password = document.querySelector('#login-password').value.trim();
 
-  if (email && password) {
+  if (username && password) {
     const response = await fetch('/api/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
   }
