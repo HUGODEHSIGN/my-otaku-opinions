4b2c667a95adce79257de63f54d2f336e6f9de8a
Nahid Ahmed <npahmed92@gmail.com>
2024-04-17 00:27:05 -0400

    validate user data

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/login', async (req, res) => {});
+router.post('/login', async (req, res) => {
+  const userData = await User.findOne({ where: { email: req.body.email } });
+
+  if (!userData) {
+    res
+      .status(400)
+      .json({ message: 'Incorrect email or password, please try again' });
+    return;
+  }
+});
