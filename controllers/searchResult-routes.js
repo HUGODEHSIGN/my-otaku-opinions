c6be9e6da88c4a9015473f4f7173d4418d25dca8
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-16 22:53:39 -0400

    filtered data to only show those which have no rating fields that are null

--- controllers/searchResult-routes.js
+++ controllers/searchResult-routes.js
@@ -6,9 +6,19 @@ router.get('/', async (req, res) => {
     // this url only gets the top 25 results, change later if we want pagination
     const queryParams = req.query.q;
     const result = await fetch(
-      `https://api.jikan.moe/v4/anime?q=${queryParams}&order_by=popularity&sort=asc`
+      `https://api.jikan.moe/v4/anime?q=${queryParams}&order_by=score&sort=desc`
     );
     const apiData = await result.json();
+    apiData.data = await apiData.data.filter(
+      (data) =>
+        data.rating &&
+        data.score &&
+        data.scored_by &&
+        data.rank &&
+        data.popularity &&
+        data.members &&
+        data.favorites
+    );
     console.log(apiData);
     res.status(200).render('searchResults', { apiData });
   } catch (err) {
