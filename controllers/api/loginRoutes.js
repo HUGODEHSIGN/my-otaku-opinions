af1ca23e5fa63efda05db04e34970446cb5f050e
Nahid Ahmed <npahmed92@gmail.com>
2024-04-17 00:35:43 -0400

    save user id and set logged in to true

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -19,4 +19,11 @@ router.post('/login', async (req, res) => {
       .json({ message: 'Incorrect email or password, please try again' });
     return;
   }
+
+  req.session.save(() => {
+    req.session.user_id = userData.id;
+    req.session.logged_in = true;
+
+    res.json({ user: userData, message: 'You are now logged in!' });
+  });
 });
