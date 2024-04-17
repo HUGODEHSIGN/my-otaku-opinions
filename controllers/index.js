0d04603ddeda651f3cef67d59bab1536fb236261
ECS <ethansantana93@gmail.com>
2024-04-17 10:52:28 -0400

    deleted category route not in use

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,6 @@ const apiRoutes = require('./api/index.js');
 const homeRoutes = require('./home-routes.js');
 const followedRoutes = require('./followed-routes.js');
 const categoryRoutes = require('./category-routes.js');
-const searchResultRoutes = require('./searchResult-routes.js');
 const signupRoutes = require('./signup-routes.js');
 const loginRoutes = require('./login-routes.js');
 
@@ -11,9 +10,7 @@ router.use('/', homeRoutes);
 router.use('/signup', signupRoutes);
 router.use('/login', loginRoutes);
 router.use('/followed', followedRoutes);
-router.use('/category', categoryRoutes);
 router.use('/search', searchResultRoutes);
-
 router.use('/api', apiRoutes);
 
 module.exports = router;
