c3b8eb105bef40f68bf3b6e284bd4566d4840da1
Nahid Ahmed <npahmed92@gmail.com>
2024-04-17 01:00:02 -0400

    create user and save session

--- controllers/api/signupRoutes.js
+++ controllers/api/signupRoutes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', async (req, res) => {});
+router.post('/', async (req, res) => {
+  const userData = await User.create(req.body);
+
+  req.session.save(() => {
+    req.session.user_id = userData.id;
+    req.session.logged_in = true;
+
+    res.status(200).json(userData);
+  });
+});
