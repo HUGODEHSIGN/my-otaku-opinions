04045aa509a8e8907ad32189e903c14da894c9b9
ECS <ethansantana93@gmail.com>
2024-04-17 11:17:43 -0400

    Change email to username

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -3,11 +3,13 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
@@ -16,7 +18,7 @@ router.post('/', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
