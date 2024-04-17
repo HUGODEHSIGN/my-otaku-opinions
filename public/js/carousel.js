04f9ea90149d6241c158833698b6de3ba1f1dd06
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-17 12:01:08 -0400

    shows 1 one main one on small devices, shows 5 on larger devices

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,14 +1,13 @@
-const emblaNode = document.querySelector('.embla');
-const options = { loop: false };
-
+const rootNode = document.querySelector('.embla');
 const viewportNode = document.querySelector('.embla__viewport');
+const options = { loop: true };
 
 const prevButtonNode = document.querySelector('.embla__prev');
 const nextButtonNode = document.querySelector('.embla__next');
 
-const emblaApi = EmblaCarousel(emblaNode, options);
+// const emblaApi = EmblaCarousel(emblaNode, options);
 
-const embla = EmblaCarousel(viewportNode);
+const embla = EmblaCarousel(viewportNode, options);
 
 prevButtonNode.addEventListener('click', embla.scrollPrev, false);
 nextButtonNode.addEventListener('click', embla.scrollNext, false);
