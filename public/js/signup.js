cd3c0ad7f638b635a3b8819bf35e69e718e23303
ECS <ethansantana93@gmail.com>
2024-04-17 10:37:27 -0400

    did the same to signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,12 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
   const username = document.querySelector('#signup-username').value.trim();
-  const email = document.querySelector('#signup-email').value.trim();
   const password = document.querySelector('#signup-password').value.trim();
 
   if (username && email && password) {
     const response = await fetch('/api/signup', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
