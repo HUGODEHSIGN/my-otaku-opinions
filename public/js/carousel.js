f1f20c5cb1bc2a21f3db5d83a3ad812c2820af90
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-17 11:52:09 -0400

    added prev and next buttons

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,5 +1,16 @@
 const emblaNode = document.querySelector('.embla');
 const options = { loop: false };
+
+const viewportNode = document.querySelector('.embla__viewport');
+
+const prevButtonNode = document.querySelector('.embla__prev');
+const nextButtonNode = document.querySelector('.embla__next');
+
 const emblaApi = EmblaCarousel(emblaNode, options);
+
+const embla = EmblaCarousel(viewportNode);
+
+prevButtonNode.addEventListener('click', embla.scrollPrev, false);
+nextButtonNode.addEventListener('click', embla.scrollNext, false);
 
 console.log(emblaApi.slideNodes()); // Access API
