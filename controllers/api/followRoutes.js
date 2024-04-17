bb78eec00af8b6802312f7109604f9adee917957
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-17 16:02:26 -0400

    created new reference to user and anime from useranime when user follows

--- controllers/api/followRoutes.js
+++ controllers/api/followRoutes.js
@@ -2,23 +2,35 @@ const router = require('express').Router();
 const { Anime, UserAnime } = require('../../models');
 
 router.post('/', async (req, res) => {
-  const malId = req.body.malId;
+  try {
+    const userId = req.session.user_id;
+    console.log('userid: ', userId);
+    const malId = req.body.malId;
 
-  const apiRes = await fetch(`https://api.jikan.moe/v4/anime/${malId}`);
-  const apiData = await apiRes.json();
-  const { images, title, synopsis } = await apiData.data;
-  console.log(apiAnime);
+    const apiRes = await fetch(`https://api.jikan.moe/v4/anime/${malId}`);
+    const apiData = await apiRes.json();
+    const { images, title, synopsis } = await apiData.data;
 
-  const [anime, created] = await Anime.findOrCreate({
-    where: { api_id: malId },
-    defaults: {
-      title,
-      synopsis,
-      img_url: images.webp.image_url,
-      api_id: malId,
-    },
-  });
-  console.log(req.body);
+    const [animeData, created] = await Anime.findOrCreate({
+      where: { api_id: malId },
+      defaults: {
+        title,
+        synopsis,
+        img_url: images.webp.image_url,
+        api_id: malId,
+      },
+    });
+    const anime = animeData.dataValues;
+
+    const userAnime = await UserAnime.create({
+      anime_id: anime.id,
+      user_id: userId,
+    });
+
+    console.log('THIS CONSOLE LOG', userAnime);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
