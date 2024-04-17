63b1359dbe09e03b88f738fb39d461538f2039e4
ECS <ethansantana93@gmail.com>
2024-04-17 10:42:15 -0400

    merge

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  const username = document.querySelector('#login-username').value.trim();
+  const username = document.querySelector('#signup-username').value.trim();
   const password = document.querySelector('#login-password').value.trim();
 
   if (username && password) {
