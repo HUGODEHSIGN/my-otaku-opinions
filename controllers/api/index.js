537bc3bf44ac4a473de032f1504b60179195f907
Lovertab <lovertabrown@gmail.com>
2024-04-17 10:18:41 -0400

    moved the logout route to api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const loginRoutes = require('./loginRoutes.js');
+const logoutRoutes = require('./logoutRoutes.js');
 
 router.use('/login', loginRoutes);
 
