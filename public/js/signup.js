30835354abc790902d4b6db49bb14084008e99a9
Nahid Ahmed <npahmed92@gmail.com>
2024-04-18 12:03:37 -0400

    validate username when signing up

--- public/js/signup.js
+++ public/js/signup.js
@@ -19,6 +19,41 @@ const signupFormHandler = async (event) => {
   }
 };
 
+// Function to validate username
+async function validateUsername(username) {
+  // const lowercaseUsername = username.toLowerCase();
+  const usernameField = document.getElementById('signup-username');
+  const usernameHelpBlock = document.getElementById('usernameHelpBlock');
+  // usernameField.value = lowercaseUsername;
+
+  const response = await fetch('/api/signup', {
+    method: 'POST',
+    body: JSON.stringify({ username }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const data = await response.json();
+
+  if (username.length > 0 && !data.usernameExists) {
+    usernameField.classList.remove('is-invalid');
+    usernameField.classList.add('is-valid');
+    usernameHelpBlock.style.display = 'none';
+    return true;
+  } else if (username.length > 0 && data.usernameExists) {
+    usernameField.classList.remove('is-valid');
+    usernameField.classList.add('is-invalid');
+    usernameHelpBlock.style.display = 'block';
+    usernameHelpBlock.textContent =
+      'Username is unavailable. Try adding numbers, letters, underscores _ , or periods.';
+    return true;
+  } else {
+    usernameField.classList.remove('is-valid');
+    usernameField.classList.add('is-invalid');
+    usernameHelpBlock.style.display = 'block';
+    usernameHelpBlock.textContent = 'Please enter a valid username.';
+    return false;
+  }
+}
+
 document
   .querySelector('#signup-form')
   .addEventListener('submit', signupFormHandler);
